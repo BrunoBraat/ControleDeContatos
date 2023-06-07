f190405f5d9dbe3abe7f0838c694ddc9a05fb4de
Bruno Braat <bruno.braat@gmail.com>
2023-06-07 11:48:29 -0300

    Criando as Views de Criar,Editar e apagar contato!

---
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult ViewIndexContato()
+        {
+            return View();
+        }
+
+        public IActionResult ViewCriarContato()
+        {
+            return View();
+        }
+
+        public IActionResult ViewEditarContato()
+        {
+            return View();
+        }
+
+        public IActionResult ViewConfirmaDeletarContato()
+        {
+            return View();
+        }
+    }
+}
